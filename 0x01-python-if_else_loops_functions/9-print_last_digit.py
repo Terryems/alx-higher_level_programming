aef6aad2826f8ebd4311b5d707e7b2eed7502dd8
Terryems <emmanuelsimona@gmail.com>
2022-11-30 01:11:06 -0800

    9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-# Author - Emmanuel Simon
+# print_last_digit: function to write the last digit of a number
+# Number: integer input
+# last: the final result
+# Return: an integer
 
 def print_last_digit(number):
-    print(abs(number) % 10, end="")
-    return (abs(number) % 10
+    if number < 0:
+        last = (-1 * number) % 10
+    else:
+        last = number % 10
+
+    print('{}'.format(last), end='')
+
+    return last
